6da26e12c1413faf349554f429d59f72f3d22a20
Alex-Potlog <alexpotloghary@gmail.com>
2026-04-28 18:42:19 +0200

    Added Vue support

---
+++ Cluedo/frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/static/',
+  build: {
+    outDir: '../static/dist',  // donde Django busca los archivos
+    emptyOutDir: true,
+    manifest: true,  // necesario para django-vite
+  },
+  server: {
+    port: 5173,
+  }
+})
